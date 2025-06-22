edb0ed513a06627772e4a13126ffc0fcc471de35
amanzhola <amanzholaimov@gmail.com>
2025-06-22 07:47:04 +0500

    Максимально использованы все возможные возможности добавить фигурные скобки и инициализации переменных по максимуму, насколько это возможно

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -15,49 +15,70 @@ bool ReadNumber(Number& result) {
 }
 
 bool RunCalculatorCycle() {
-    Number value = 0, memory = 0;
+    Number value = 0.0;
+    Number memory = 0.0;
     bool memorySet = false;
+    char cmd = '\0';
 
-    if (!ReadNumber(value)) { return false;}
+    if (!ReadNumber(value)) {
+        return false;
+    }
 
-    char cmd;
     while (std::cin >> cmd) {
         if (cmd == '*' && std::cin.peek() == '*') {
             std::cin.get();
-            Number x;
-            if (!ReadNumber(x)) { return false;}
+            Number x = 0.0;
+            if (!ReadNumber(x)) {
+                return false;
+            }
             value = std::pow(value, x);
             continue;
         }
 
         switch (cmd) {
             case '+': case '-': case '*': case '/': case ':': {
-                Number x;
-                if (!ReadNumber(x)) { return false;}
+                Number x = 0.0;
+                if (!ReadNumber(x)) {
+                    return false;
+                }
                 switch (cmd) {
-                    case '+': value += x; break;
-                    case '-': value -= x; break;
-                    case '*': value *= x; break;
-                    case '/': value = (x == 0 ? std::numeric_limits<Number>::infinity() : value / x); break;
-                    case ':': value = x; break;
+                    case '+': { value += x; break; }
+                    case '-': { value -= x; break; }
+                    case '*': { value *= x; break; }
+                    case '/': { value = (x == 0 ? std::numeric_limits<Number>::infinity() : value / x); break; }
+                    case ':': { value = x; break; }
                 }
                 break;
             }
-            case '=': std::cout << value << '\n'; break;
-            case 'c': value = 0; break;
-            case 's': memory = value; memorySet = true; break;
-            case 'l':
+            case '=': {
+                std::cout << value << '\n';
+                break;
+            }
+            case 'c': {
+                value = 0.0;
+                break;
+            }
+            case 's': {
+                memory = value;
+                memorySet = true;
+                break;
+            }
+            case 'l': {
                 if (!memorySet) {
                     std::cerr << "Error: Memory is empty\n";
                     return false;
                 }
                 value = memory;
                 break;
-            case 'q': return true;
+            }
+            case 'q': {
+                return true;
+            }
             default: {
                 std::string token(1, cmd);
-                while (std::isalpha(std::cin.peek()))
+                while (std::isalpha(std::cin.peek())) {
                     token += std::cin.get();
+                }
                 std::cerr << "Error: Unknown token " << token << '\n';
                 return false;
             }
